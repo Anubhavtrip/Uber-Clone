93e156105436bab27290344ca61d7c3969706018
anubhav <abhisubh47@gmail.com>
2024-12-28 17:50:44 +0530

    remove warning

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 
 //connect Database
 function connectToDb (){
-    mongoose.connect(process.env.DB_CONNECT,{
-        useNewURLParser:true,
-        useUnifiedTopology:true
-    }).then(()=>console.log("Connect database"))
+    mongoose.connect(process.env.DB_CONNECT).then(()=>console.log("Connect database"))
     .catch(err => console.log(err));
 }
 
